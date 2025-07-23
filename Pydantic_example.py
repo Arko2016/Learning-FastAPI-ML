a1d03f6aa79abda5d358132081bd12f546247732
Arko2016 <arkojyoti.hore@gmail.com>
2025-07-22 23:46:35 -0700

    small changes

--- Pydantic_example.py
+++ Pydantic_example.py
@@ -1,20 +1,31 @@
-from pydantic import BaseModel
-from typing import List, Dict, Optional
+from pydantic import BaseModel, EmailStr, AnyUrl, Field
+from typing import List, Dict, Optional, Annotated
+
+'''
+Field + Annotated functions help in 3 ways:
+1. specify data type
+2. provide default values
+3. provide details about the attribute
+'''
 
 #create a Pydantic model using BaseModel to define the required variables
 class Patient(BaseModel):
-    name: str
-    age: int
-    weight: float
-    married: bool = False #a default value can be specified
+    name: str = Annotated[str, Field(max_length=50, title = 'Name of the patient', description='Give name of patient in less than 50 characters', examples=['ajh','ms'])] #sets contraint on string length
+    age: int = Field(gt = 0, lt = 100) #age has to be within range of 0 to 120
+    email: EmailStr #this is a Pydantic datatype format to help validate emails
+    linkedin_url: AnyUrl #Pydantic datatype to validate links
+    weight: Annotated[float, Field(gt = 0, strict=True)] #helps to set constraints, in this case indicates weight cannot be less than 0. strict ensures data type coercion does not occur
+    married: Annotated[bool, Field(default=None, description="Is the patient married? enter True or False")] #a default value can be specified
     #make the allergies variable optional
-    allergies: Optional[List[str]] = None #an optional variable should always be given a default value of None
+    allergies: Annotated[Optional[List[str]], Field(default = None, max_length = 5)] #an optional variable should always be given a default value of None
     contact_details: Dict[str,str] # denotes that both key and value for dictionary should be string
 
 #define a function with the pydantic object as input parameter
 def update_patient_data(patient: Patient):
     print(patient.name)
     print(patient.age)
+    print(patient.email)
+    print(patient.linkedin_url)
     print(patient.weight)
     print(patient.married)
     print(patient.allergies)
@@ -22,7 +33,7 @@ def update_patient_data(patient: Patient):
     print('updated')
 
 #create a json object to be used to instantiate the Pydantic model
-patient_info = {'name': 'aj', 'age': 35, 'weight': 180, 'contact_details': {'phone':'232323'}}
+patient_info = {'name': 'aj', 'age': 35,'email': 'abc@xyz.com', 'weight': 180, 'linkedin_url':'http:linkedin.com/1322', 'contact_details': {'phone':'232323'}}
 
 #instantiate pydantic object
 #Note: since the patient_info is in json format, need to unpack it
